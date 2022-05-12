41122377889b4c3270e4352be8900449af86f28c
Aleksandrs Lukins <al20101@edu.rta.lv>
2022-05-12 16:15:48 +0300

    add col study year

--- crud_student/models.py
+++ crud_student/models.py
@@ -4,6 +4,7 @@ from django.db import models
 class Students(models.Model):
     name = models.CharField(max_length=30)
     surname = models.CharField(max_length=30)
+    study_year = models.IntegerField(default= 2022)
 
     def __str__(self):
         return "%s" %(self.name)
